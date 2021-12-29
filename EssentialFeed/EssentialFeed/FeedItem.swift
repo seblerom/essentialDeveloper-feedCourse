9bbb204288f7303a331f9d3b8aff1302171d429c
Sebastian Leon <sebastian.leon@strv.com>
2021-12-29 14:08:56 +0100

    Create the `FeedItem` model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Sebastian Leon on 29.12.2021.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let iamgeURL: URL
+	let description: String?
+	let location: String?
+}
