2ee3388e684f8b06163a9ff67eca68168b27017a
Sebastian leon <soloban88@gmail.com>
2022-03-17 15:15:12 +0100

    Replace custom `HTTPClientResult` enum with a nested type alias over the standard `Swift.Result`

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -197,9 +197,9 @@ private extension LoadFeedFromRemoteUseCaseTests {
             messages.map { $0.url }
         }
         
-        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
         
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url, completion))
         }
         
