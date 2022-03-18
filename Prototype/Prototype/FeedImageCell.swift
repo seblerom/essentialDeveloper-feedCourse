32cad9f97541e650a3b76d67056e5fc8bd0542b5
Sebastian leon <soloban88@gmail.com>
2022-03-18 12:31:53 +0100

    Add  with storyboard connections

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Sebastian Leon on 3/18/22.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+
+    @IBOutlet private(set) var locationContainer: UIView!
+    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet private(set) var feedImageView: UIImageView!
+    @IBOutlet private(set) var descriptionLabel: UILabel!
+
+}
